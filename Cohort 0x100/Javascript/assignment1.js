38e42b4ca7545fea8746452f92f2a05de4044196
Ashiii27 <ashiii.2790@gmail.com>
2026-06-05 09:28:02 +0530

    problem2 solved

--- Cohort 0x100/Javascript/assignment1.js
+++ Cohort 0x100/Javascript/assignment1.js
@@ -1,5 +1,6 @@
 //Create a counter in JS that counts from 30 to 0
 
+/*
 let count = 30;
 
 const interval = setInterval(() => {
@@ -11,3 +12,17 @@ const interval = setInterval(() => {
     console.log("Done!");
   }
 }, 1000);
+
+*/
+
+//calculate the time it takes  between a setTimeout call and the inner function actually running
+
+const start = Date.now();
+
+setTimeout(() => {
+  const actual = Date.now() - start;
+  console.log(`Requested delay: 2000ms`);
+  console.log(`Actual delay: ${actual}ms`);
+  console.log(`Drift: ${actual - 2000}ms`);
+}, 2000);
+
