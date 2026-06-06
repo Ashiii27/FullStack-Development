c42e8104ed6525e99bf31fef48b2ff9f40f32609
Ashiii27 <ashiii.2790@gmail.com>
2026-06-06 20:17:21 +0530

    replace funtion in strings

--- Cohort 0x100/Javascript/Day2.js
+++ Cohort 0x100/Javascript/Day2.js
@@ -13,4 +13,17 @@ function findIndexOf(str, target) {
     console.log("index : " , str.indexOf(target));
 }
 
-findIndexOf("ashish", "sh");
+// findIndexOf("ashish", "sh");
+
+function getSlice(str, start, end) {
+    console.log("original string : ", str);
+    console.log("sliced string : ", str.slice(start, end));
+    
+}
+
+// getSlice("ashish", 5, 0);
+
+
+//replace
+// const str = "hello world";
+// console.log(str.replace("world", "javascript"));
