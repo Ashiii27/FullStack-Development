2b492504efd8ba589a3687024ff523fdeb35d576
Ashiii27 <ashiii.2790@gmail.com>
2026-06-06 13:11:21 +0530

    getindex function

--- Cohort 0x100/Javascript/Day2.js
+++ Cohort 0x100/Javascript/Day2.js
@@ -1,5 +1,16 @@
-//Different string operations in JavaScript
+//Different string operations in JavaScript (auxiliary helping methods/functions)
 
 function getLength(str) {
     return str.length;
 }
+
+// let a = "ashish";
+// let length = getLength(a);
+// console.log(length);
+
+function findIndexOf(str, target) {
+    console.log("original string : ", str);
+    console.log("index : " , str.indexOf(target));
+}
+
+findIndexOf("ashish", "sh");
