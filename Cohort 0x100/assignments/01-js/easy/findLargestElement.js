3c0f05281df3dfab39e60d51fb9683e686a308e3
Ashiii27 <ashiii.2790@gmail.com>
2026-06-07 09:25:05 +0530

    solved largestElement by for loop

--- Cohort 0x100/assignments/01-js/easy/findLargestElement.js
+++ Cohort 0x100/assignments/01-js/easy/findLargestElement.js
@@ -5,10 +5,21 @@
   - Output: 9
 */
 
-function findLargestElement(numbers) {
-    let largest = Math.max(...numbers);
-    return largest;
+// function findLargestElement(numbers) {
+//     let largest = Math.max(...numbers);
+//     return largest;
     
+// }
+
+function findLargestElement(numbers) {
+    
+    let largest = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > largest) {
+            largest = numbers[i];
+        }
+    }
+    return largest;
 }
 
 let largestElement = findLargestElement([3, 7, 2, 9, 1]);
